edb3bbb0784006db11c64b3c7d5344b89b8c9e5c
Bharathiselvan <bharathiselvan_vasudevan@trimble.com>
2020-05-31 12:28:54 +0530

    no message

--- src/main/java/com/headspin/hackathon/pages/Home.java
+++ src/main/java/com/headspin/hackathon/pages/Home.java
@@ -1,6 +1,9 @@
 package com.headspin.hackathon.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.headspin.hackathon.core.setup.BasePage;
@@ -15,5 +18,38 @@ public class Home extends BasePage {
 	public void loadHomePage(String url) {
 		loadURL(url);
 	}
+
+	@FindBy(xpath = "//p[contains(., \"Login\")]")
+	WebElement login;
+
+	@FindAll({ @FindBy(id = "username"), @FindBy(xpath = "//input[@id='username']") })
+	WebElement username;
+
+	@FindAll({ @FindBy(xpath = "//button[@class='capText font16']/span[contains(text(), 'Continue')]"),
+			@FindBy(xpath = "//span[contains(text(), 'Continue')]") })
+	WebElement continueBtn;
+
+	@FindAll({ @FindBy(id = "password"), @FindBy(xpath = "//input[@id='password']") })
+	WebElement password;
+
+	@FindAll({ @FindBy(xpath = "//button[@class='capText font16']/span[contains(text(), 'Login')]"),
+			@FindBy(xpath = "//span[contains(text(), 'Login')]") })
+	WebElement loginBtn;
+
+	@FindBy(xpath = "//div[@class='modalClose pushRight']/span")
+	WebElement modalClose;
+
+	@FindAll({
+			@FindBy(xpath = "//a[@href='https://www.makemytrip.com/hotels/' and @class=' makeFlex hrtlCenter column']"),
+			@FindBy(partialLinkText = "hotels") })
+	WebElement hotels;
+
+	@FindAll({ @FindBy(id = "city"), @FindBy(xpath = "//input[@id='city' and @type='text']") })
+	WebElement cityName;
+	
+	@FindBy(xpath = "//input[@placeholder='Enter city/ Hotel/ Area/ Building' and @type='text']")
+	WebElement enteCityName;
+	
+	
 
 }
